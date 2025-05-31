f8e4a40c7a5a491631a65c3240c9aefd5ba7b058
Khushi Roy <roykhushi432@gmail.com>
2025-06-01 01:16:34 +0530

    experience

--- components/Bento.tsx
+++ components/Bento.tsx
@@ -9,6 +9,7 @@ import { BracesIcon } from "lucide-react";
 import Link from "next/link";
 import { SparklesText } from "./magicui/sparkles-text";
 import { BackgroundBeamsWithCollision } from "./ui/background-beams-with-collision";
+import { Button } from "./ui/button";
 
 export function BentoGridThirdDemo() {
   return (
@@ -40,7 +41,7 @@ const SkeletonOne = () => {
         </div>
         <span className="text-4xl font-bold">Khushi Roy.</span>
 
-        <p className="text-sm text-gray-500">@royKhushi27</p>
+        <Button>Resume</Button>
       </div>
 
       <div className="mt-4 pt-4 flex items-center gap-2 p-2">
@@ -61,42 +62,44 @@ const SkeletonOne = () => {
 };
 
 const SkeletonTwo = () => {
-  const variants = {
-    initial: {
-      width: 0,
-    },
-    animate: {
-      width: "100%",
-      transition: {
-        duration: 0.2,
-      },
-    },
-    hover: {
-      width: ["0%", "100%"],
-      transition: {
-        duration: 2,
-      },
-    },
-  };
-  const arr = new Array(6).fill(0);
   return (
-    <motion.div
-      initial="initial"
-      animate="animate"
-      whileHover="hover"
-      className="flex flex-1 w-full h-full min-h-[6rem] dark:bg-dot-white/[0.2] bg-dot-black/[0.2] flex-col space-y-2"
-    >
-      {arr.map((_, i) => (
-        <motion.div
-          key={"skelenton-two" + i}
-          variants={variants}
-          style={{
-            maxWidth: Math.random() * (100 - 40) + 40 + "%",
-          }}
-          className="flex flex-row rounded-full border border-neutral-100 dark:border-white/[0.2] p-2  items-center space-x-2 bg-neutral-100 dark:bg-black w-full h-4"
-        ></motion.div>
-      ))}
-    </motion.div>
+    <div>
+      <div>
+        <h1 className="text-2xl md:text-4xl text-center font-bold italic underline decoration-blue-600 decoration-wavy">
+          Experience
+        </h1>
+      </div>
+      <div className="flex items-center p-2 gap-2">
+        <h2 className="">AKB Tech</h2>
+        <span className="">|</span>
+        <h2>Front-End Developer</h2>
+      </div>
+      <div className="flex items-center p-2 ml-2">
+        <p className="text-xs">October 2024-December 2024</p>
+      </div>
+      <div className="flex items-center p-2">
+        <ul className="list-disc mx-auto">
+          <li className="text-sm">
+            Designed & optimized responsive UIs for the company’s website and
+            admin dashboards using React.js, Tailwind CSS, Material UI, and
+            Framer Motion.
+          </li>
+          <li className="text-sm">
+            Optimized state management with Zustand, reducing re-renders by 30%,
+            and integrated OTPless authentication, cutting login time by 40%.
+          </li>
+          <li className="text-sm">
+            Enhanced website performance by 44% through lazy loading, SEO
+            enhancements and code enhancements, and code optimization, reducing
+            page load time by 1.5 seconds.
+          </li>
+          <li className="text-sm">
+            Collaborated in regular code reviews ensuring code quality,
+            maintainability, and scalability.
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
@@ -151,7 +154,7 @@ const SkeletonFour = () => {
   return (
     <div>
       <div className="flex items-center mb-4 p-2">
-        <h1 className="text-3xl md:text-4xl font-bold tracking-tight">
+        <h1 className="text-3xl md:text-4xl font-bold tracking-tight underline decoration-wavy decoration-blue-600">
           TECH STACK
         </h1>
       </div>
@@ -186,7 +189,9 @@ const SkeletonFour = () => {
         </div>
       </div>
       <div className="flex items-center gap-2 mt-4">
-        <span className="tracking-tight font-semibold dark:border-gray-600">Db:</span>
+        <span className="tracking-tight font-semibold dark:border-gray-600">
+          Db:
+        </span>
         <div className="border rounded-full p-2 dark:border-gray-600">
           <p className="text-xs">MongoDB</p>
         </div>
@@ -241,7 +246,6 @@ const SkeletonFive = () => {
 
 const items = [
   {
-    
     description: (
       <div className="flex items-center justify-between">
         <span className="text-sm">
@@ -271,31 +275,20 @@ const items = [
     header: <SkeletonFive />,
     className: "md:col-span-1 cursor-pointer ",
   },
-  //   {
-  //     title: "Automated Proofreading",
-  //     description: (
-  //       <span className="text-sm">
-  //         Let AI handle the proofreading of your documents.
-  //       </span>
-  //     ),
-  //     header: <SkeletonTwo />,
-  //     className: "md:col-span-1",
-  //     icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
-  //   },
+
   {
-    
     header: <SkeletonFour />,
     className: "md:col-span-2 ",
   },
   {
-    title: "Automated Proofreading",
-    description: (
-      <span className="text-sm">
-        Let AI handle the proofreading of your documents.
-      </span>
-    ),
+    // title: "Automated Proofreading",
+    // description: (
+    //   <span className="text-sm">
+    //     Let AI handle the proofreading of your documents.
+    //   </span>
+    // ),
     header: <SkeletonTwo />,
     className: "md:col-span-3",
-    icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
+    // icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
   },
 ];
